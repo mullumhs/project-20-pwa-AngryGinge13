10be09db7898790b64344e99ce791d8f4bfbb1f6
AngryGinge13 <Sonny.mcgauley@education.nsw.gov.au>
2025-11-12 10:38:54 +1100

    database model setup: added make,model, color, year, and odometer.
    Started on add route (Not finished)
    
    added add html and used old code for html page.
    
    created index html code with color background and printing the database.
    
    12/11/25

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from views import init_routes
 # Create the Flask app and configure it
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///collection.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cars.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialise the database and routes
@@ -16,3 +16,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+    
--- models.py
+++ models.py
@@ -1,6 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cars.db'
+db = SQLAlchemy(app)
 
-# Define your database model here
-# Example: class Item(db.Model):
+
+
+class Cars(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String)
+    model = db.Column(db.String)
+    color = db.Column(db.String)
+    year = db.Column(db.Integer)
+    odometer = db.Column(db.Integer)
+    
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+    
+@app.route('/add', methods=['GET', 'POST'])
+def add_car():
+    if request.method == 'POST':
+        new_car = Cars(
+            make=request.form['make'],
+            model=request.form['model'],
+            color=request.form['color'],
+            year=int(request.form['year']),
+            odometer=int(request.form['odometer'])
+        )
+        db.session.add(new_car)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
