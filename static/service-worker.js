28cba88750418d5c86eeb6721d2d1dbf1940cb65
AngryGinge13 <Sonny.mcgauley@education.nsw.gov.au>
2025-12-18 09:58:59 +1100

    changed css into static css folder, added icon and installiblity for cars.DB added manifest and service worker

---
+++ static/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "Cars-app-v1";
+
+const urlsToCache = [
+  "/",
+
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js",
+
+  "/static/css/base.css",
+
+  "/static/images/icon-192x192.png",
+
+  "/static/images/icon-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request);
+      })
+  );
+});
